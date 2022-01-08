fee1cbc3d896f0343b70af686b0ba8c6a7240832
josef <jventura@dm5i.com>
2022-01-08 13:27:05 -0300

    login view

---
+++ Inco/src/Components/LoginForm/Index.jsx
@@ -0,0 +1,21 @@
+import './login.css';
+const Login = ()=>{
+
+
+    return (
+        <div className="Login">
+            <div className="Login-Form">
+                <h3 className="Title">Login</h3>
+                <form className="Form">
+                    <span className='Input-Span'><label className="Text">User</label><input type="text" className="Input-Text" id="user"/></span>
+                    <span className='Input-Span'><label className="Text">Password</label><input type="text" className="Input-Text" id="password"/></span>
+                    <button className="Button">Log In</button>
+                </form>
+            </div>
+        </div>
+    )
+    
+}
+
+
+export default Login
