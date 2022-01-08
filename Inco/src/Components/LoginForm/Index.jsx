29fb87a76a3d292d6d73e40574ec0c1db1715d50
josef <jventura@dm5i.com>
2022-01-08 13:31:53 -0300

    fix

--- Inco/src/Components/LoginForm/Index.jsx
+++ Inco/src/Components/LoginForm/Index.jsx
@@ -7,8 +7,14 @@ const Login = ()=>{
             <div className="Login-Form">
                 <h3 className="Title">Login</h3>
                 <form className="Form">
-                    <span className='Input-Span'><label className="Text">User</label><input type="text" className="Input-Text" id="user"/></span>
-                    <span className='Input-Span'><label className="Text">Password</label><input type="text" className="Input-Text" id="password"/></span>
+                    <span className='Input-Span'>
+                        <label className="Text">User</label>
+                        <input type="text" className="Input-Text" id="user"/>
+                    </span>
+                    <span className='Input-Span'>
+                        <label className="Text">Password</label>
+                        <input type="password" className="Input-Text" id="password"/>
+                    </span>
                     <button className="Button">Log In</button>
                 </form>
             </div>
